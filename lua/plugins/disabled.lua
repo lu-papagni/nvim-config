35f7a7ba1995791b6319589b0a9e1a298bbb78be
lu-papagni <papagniluca4@gmail.com>
2024-07-19 19:36:41 +0200

    Disabilitato flash.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
-  -- { "folke/noice.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
