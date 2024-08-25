c99ceeb664578cfd33b27d82551e3976b56131a7
lu-papagni <papagniluca4@gmail.com>
2024-08-25 14:27:45 +0200

    Disabilitato noice.nvim

--- lua/plugins/00-disable.lua
+++ lua/plugins/00-disable.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
 }
