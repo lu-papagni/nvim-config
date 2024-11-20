14110dad8f8630dcabc9aa607041dab06fc007e0
lu-papagni <papagniluca4@gmail.com>
2024-11-20 20:06:28 +0100

    Aggiunte mappature custom

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Copia/incolla nel buffer di sistema
+-- stylua: ignore
+map({ "n", "v" }, "<A-y>", "\"+y", { desc = "Copia nel buffer di sistema", noremap = true })
+-- stylua: ignore
+map({ "n", "v" }, "<A-p>", "\"+p", { desc = "Incolla dal buffer di sistema", noremap = true })
+
+-- Rimappature specifiche di Neovide
+if vim.g.neovide then
+  -- Mette Neovide a schermo intero
+  map("n", "<F11>", function()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end, { desc = "Metti Neovide a schermo intero" })
+end
--- lua/plugins/_disable.lua
+++ lua/plugins/_disable.lua
@@ -4,4 +4,13 @@ return {
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "<a-p>", false }
+    end,
+  },
 }
