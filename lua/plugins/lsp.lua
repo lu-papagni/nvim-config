f2e86fda43b70ba067e857ec1dcefc086aeafe2f
lu-papagni <papagniluca4@gmail.com>
2024-12-01 19:10:09 +0100

    Inizio migrazione da LSP gestiti con `mason` a manuale
    Configurazione mancante per `jdtls`

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,83 +1,99 @@
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      {
-        "neovim/nvim-lspconfig",
-        lazy = true,
-        dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    event = { "BufNewFile", "BufReadPost", "VeryLazy" },
+    -- NOTE: uso opzioni custom
+    opts = {
+      server_settings = {
+        rust_analyzer = {},
+        clangd = function()
+          local cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=never",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+            "--function-arg-placeholders=false"
+          }
+
+          local gcc = vim.fn.exepath("gcc"):gsub("%.EXE$", ".exe")
+          local gpp = vim.fn.exepath("g++"):gsub("%.EXE$", ".exe")
+
+          vim.list_extend(cmd, {
+            table.concat { "--query-driver=", gcc },
+            table.concat { "--query-driver=", gpp }
+          })
+
+          return { cmd = cmd }
+        end,
+        pyright = {},
+        ts_ls = {
+          init_options = {
+            preferences = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            }
+          }
+        }
+      },
+      override_behavior = {
+        ["textDocument/hover"] = vim.lsp.with(
+          vim.lsp.handlers.hover,
+          {
+            border = "single",
+            max_width = 80,
+            max_height = 15
+          }
+        )
       },
     },
-    event = { "BufReadPre", "VeryLazy" },
-    opts = function()
-      local cmp_lsp = require("cmp_nvim_lsp")
+    config = function(_, opts)
+      local lsp = require("lspconfig")
+
       local capabilities = vim.tbl_deep_extend(
         "force",
-        {},
         vim.lsp.protocol.make_client_capabilities(),
-        cmp_lsp.default_capabilities()
+        require("cmp_nvim_lsp").default_capabilities()
       )
 
-      return {
-        ensure_installed = {
-          "ts_ls",
-          "jedi_language_server",
-          "clangd",
-        },
-        handlers = {
-          function(server)
-            if server ~= "jdtls" then
-              require("lspconfig")[server].setup { capabilities = capabilities }
-            end
-          end,
-          ["ts_ls"] = function()
-            require("lspconfig").ts_ls.setup {
-              capabilities = capabilities,
-              init_options = {
-                preferences = {
-                  includeInlayParameterNameHints = "all",
-                  includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                  includeInlayFunctionParameterTypeHints = true,
-                  includeInlayVariableTypeHints = true,
-                  includeInlayPropertyDeclarationTypeHints = true,
-                  includeInlayFunctionLikeReturnTypeHints = true,
-                  includeInlayEnumMemberValueHints = true,
-                }
+      for name, settings in pairs(opts.server_settings) do
+        local server = lsp[name].config_def.default_config.cmd[1]
+
+        -- NOTE: `settings` può essere una funzione o una tabella
+        if type(settings) == "function" then settings = settings() end
+
+        if vim.fn.executable(server) == 1 then
+          lsp[name].setup(
+            vim.tbl_deep_extend(
+              "force",
+              settings,
+              {
+                handlers = opts.override_behavior,
+                capabilities = capabilities
               }
-            }
-          end,
-          ["clangd"] = function()
-            local cmd = {
-              "clangd",
-              "--background-index",
-              "--clang-tidy",
-              "--header-insertion=iwyu",
-              "--completion-style=detailed",
-              "--function-arg-placeholders",
-              "--fallback-style=llvm",
-            }
-
-            local gcc = vim.fn.exepath("gcc"):gsub("%.EXE$", ".exe")
-            local gpp = vim.fn.exepath("g++"):gsub("%.EXE$", ".exe")
-
-            vim.list_extend(cmd, {
-              table.concat { "--query-driver=", gcc },
-              table.concat { "--query-driver=", gpp }
-            })
-
-            require("lspconfig").clangd.setup {
-              capabilities = capabilities,
-              cmd = cmd,
-              init_options = {
-                usePlaceholders = true,
-                completeUnimported = true,
-                clangdFileStatus = true,
-              },
-            }
-          end
-        }
-      }
+            )
+          )
+        end
+      end
     end
   },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {
+      doc_lines = 5,
+      floating_window = true,
+      handler_opts = { border = "single" },
+      hint_enable = false,
+      hint_prefix = "\u{ea92} ",
+    }
+  }
 }
