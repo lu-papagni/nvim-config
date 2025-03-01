13f774928ac155e6cf0347e391d06f2280e49284
lu-papagni <papagniluca4@gmail.com>
2025-03-01 19:43:01 +0100

    chore(autocmd): rimosso fix per resettare lampeggiamento del cursore
    Ora il fix viene applicato direttamente dalla shell.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,10 +6,7 @@ autocmd("VimLeave", {
   desc = "Resetta l'aspetto del terminale all'uscita",
   callback = function()
     if os.getenv("TERM") == "xterm-kitty" then
-      -- vim.system({ "kitten", "@", "set-colors", "--reset", "--all" }, { detach = true })
       vim.system({ "kitten", "@", "set-spacing", "padding=default" }, { detach = true })
-    elseif vim.fn.has("wsl") == 1 then
-      vim.cmd [[set guicursor= | call chansend(v:stderr, "\x1b[ q")]]
     end
   end,
 })
