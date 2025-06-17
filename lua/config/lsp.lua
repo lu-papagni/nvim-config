54f6ad5b3c7d2099e8ca78bdbfc4cd50c1406604
lu-papagni <papagniluca4@gmail.com>
2025-06-17 14:13:46 +0200

    chore(lsp): esegui la configurazione se necessario

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -21,46 +21,49 @@ local function get_clangd_cmd()
   return cmd
 end
 
--- Configurazione language server
-vim.lsp.config("clangd", { cmd = get_clangd_cmd() })
+if vim.g.git_editor ~= 1 then
+  vim.api.nvim_create_autocmd(
+    { "BufReadPre", "BufNewFile", "UIEnter" },
+    {
+      desc = "Configura LSP al momento giusto",
+      once = true,
+      callback = function()
+        -- Configurazione language server
+        vim.lsp.config("clangd", { cmd = get_clangd_cmd() })
 
-vim.lsp.config("typescript-language-server", {
-  init_options = {
-    locale = "it",
-    preferences = {
-      includeInlayParameterNameHints = "all",
-      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-      includeInlayFunctionParameterTypeHints = true,
-      includeInlayVariableTypeHints = true,
-      includeInlayPropertyDeclarationTypeHints = true,
-      includeInlayFunctionLikeReturnTypeHints = true,
-      includeInlayEnumMemberValueHints = true,
+        vim.lsp.config("typescript-language-server", {
+          init_options = {
+            locale = "it",
+            preferences = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            }
+          }
+        })
+
+        vim.lsp.config("rust-analyzer", {
+          settings = {
+            ["rust-analyzer"] = {
+              completion = {
+                callable = { snippets = "add_parentheses" }
+              }
+            }
+          }
+        })
+
+        -- Abilito tutti
+        vim.lsp.enable {
+          "clangd",
+          "rust-analyzer",
+          "typescript-language-server",
+          "pyright"
+        }
+      end
     }
-  }
-})
-
-vim.lsp.config("rust-analyzer", {
-  settings = {
-    ["rust-analyzer"] = {
-      completion = {
-        callable = { snippets = "add_parentheses" }
-      }
-    }
-  }
-})
-
--- Abilito tutti
-vim.lsp.enable {
-  "clangd",
-  "rust-analyzer",
-  "typescript-language-server",
-  "pyright"
-}
-
--- Attivazione completamento automatico
---[[ vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
-  callback = function(ev)
-    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf)
-  end,
-}) ]]--
+  )
+end
