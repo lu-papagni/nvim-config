a7c306e30582fe0cd149a58d058f1a601cfa2afd
lu-papagni <papagniluca4@gmail.com>
2025-06-17 12:36:20 +0200

    feat(mini): sostituiti plugin più lenti con `mini`

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -41,4 +41,50 @@ return {
       }
     }
   },
+  {
+    "echasnovski/mini.indentscope",
+    version = false,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "fzf",
+          "help",
+          "lazy",
+          "qf"
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+    event = { "BufNewFile", "BufReadPost" },
+    opts = {
+      draw = {
+        -- Stessa funzione di `mini.indentscope.gen_animation.none`
+        -- Copiata per evitare di caricare il plugin
+        animation = function() return 0 end
+      },
+      options = { try_as_border = true },
+      symbol = "│"
+    }
+  },
+  {
+    "echasnovski/mini.diff",
+    version = false,
+    cond = vim.g.git_editor ~= 1,
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      {
+        "<leader>gd",
+        function() require("mini.diff").toggle_overlay() end,
+        desc = "Mostra differenze con l'indice della repository"
+      }
+    },
+    opts = {
+      view = {
+        style = "sign",
+        priority = 1
+      }
+    }
+  }
 }
