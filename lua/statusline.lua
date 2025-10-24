e7408c0b7d08ca5178723a356e4543e7935bf0ee
lu-papagni <papagniluca4@gmail.com>
2025-10-24 16:13:07 +0200

    feat(statusline): aggiunte icone di stato
    Icone per:
    - Filetype
    - Copilot attivo

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,11 +1,40 @@
 local StatusLine = {}
 
-local function language_info()
-  local lang = vim.bo.filetype
-  if lang:len() == 0 then return "" end
-  local _, lsp = next(vim.lsp.get_clients { bufnr = 0 })
-  if lsp then lang = string.format("%s%%#NonText#@%%#Special#%s%%*", lang, lsp.name) end
-  return string.format("[%s]", lang)
+local function file_icon_and_name()
+  local fname = vim.fn.expand("%:t")
+  if fname:len() > 0 then 
+    local icon, hl, _ = require("mini.icons").get("file", fname)
+    return string.format("%%#%s#%s%%* %s", hl, icon, fname)
+  end
+  return "[No Name]"
+end
+
+local function tools_info()
+  local ft = vim.bo.filetype
+  if ft:len() == 0 then return "" end
+
+  local lsp_name = nil
+  local copilot_attached = false
+
+  for _, client in pairs(vim.lsp.get_clients { bufnr = 0 }) do
+    if client.name:lower() == "copilot" then
+      copilot_attached = true
+    elseif not lsp_name then
+      lsp_name = client.name
+    end
+  end
+
+  local parts = { ft }
+  if lsp_name then
+    table.insert(parts, string.format("%%#NonText#@%%#Special#%s%%*", lsp_name))
+  end
+
+  local result = string.format("[%s]", table.concat(parts, ""))
+  if copilot_attached then
+    result = result .. " \u{f4b8}"
+  end
+
+  return result
 end
 
 local function diagnostic_count()
@@ -20,13 +49,16 @@ local function diagnostic_count()
   return table.concat(status, " ")
 end
 
+
 function StatusLine.tostring()
   return table.concat {
-    "%<%f %#WarningMsg#%m%*",
+    "%<",
+    file_icon_and_name(),
+    " %#WarningMsg#%m%*",
     string.format("%%5.(%s%%)", diagnostic_count()),
     "%=",
-    language_info(),
-    "%9.(%l%#NonText#:%*%L%)",
+    tools_info(),
+    "%12.(%l%#NonText#:%*%L%)",
   }
 end
 
