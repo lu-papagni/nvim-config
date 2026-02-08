27446c862867cf2e512b874a15261871efbfb5f2
lu-papagni <papagniluca4@gmail.com>
2026-02-08 19:57:37 +0100

    chore(mason): removed `tree-sitter-cli` install
    Now handled by chezmoi.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,4 @@
 return {
   "mason-org/mason.nvim",
-  lazy = false,
-  config = function()
-    require("mason-registry").update()
-    require("mason").setup()
-
-    local ensure_installed = { "tree-sitter-cli" }
-    local registry = require("mason-registry")
-    vim.iter(ensure_installed)
-      :filter(function(id) return not registry.is_installed(id) end)
-      :each(function(pkg) registry.get_package(pkg):install() end)
-  end
+  opts = {}
 }
