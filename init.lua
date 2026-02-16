7584c471be65090b42513fe2eba8a30afbb46dbd
lu-papagni <papagniluca4@gmail.com>
2026-02-17 00:29:05 +0100

    refactor: changed structure of key mappings

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
-require("config.keymap")
+vim.g.mapleader = vim.keycode("<Space>")
+
+for _, map in ipairs(require("config.mappings")) do
+  vim.keymap.set(map.mode or "n", map.keys, map.exec, { desc = map[1] })
+end
+
 require("config.options")
 require("config.autocmd")
 require("config.usercmd")
