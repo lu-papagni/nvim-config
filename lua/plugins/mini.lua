014d7f7365f59ac2672eed2fe9f9c4e3e435f9b9
lu-papagni <papagniluca4@gmail.com>
2026-02-16 22:37:42 +0100

    feat: use indent guides from blink
    Retired mini.ai

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -42,41 +42,6 @@ return {
     }
   },
   {
-    "echasnovski/mini.indentscope",
-    version = false,
-    init = function()
-      -- Disabilita in specifici file e nel terminale integrato
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "fzf",
-          "help",
-          "lazy",
-          "qf",
-          "codecompanion"
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-
-      vim.api.nvim_create_autocmd("TermOpen", {
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end
-      })
-    end,
-    event = { "BufNewFile", "BufReadPost" },
-    opts = {
-      draw = {
-        -- Stessa funzione di `mini.indentscope.gen_animation.none`
-        -- Copiata per evitare di caricare il plugin
-        animation = function() return 0 end
-      },
-      options = { try_as_border = true },
-      symbol = "│"
-    }
-  },
-  {
     "echasnovski/mini.diff",
     version = false,
     cond = vim.g.git_editor ~= 1,
