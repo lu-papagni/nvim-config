ba6007b3269ffd2f81b112377e24d1807c59a03e
lu-papagni <papagniluca4@gmail.com>
2026-05-31 20:16:30 +0200

    feat: better statusline
    - Don't update lsp when scrolling
    - Use builtin diagnostics status

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,7 +1,14 @@
 local StatusLine = {}
-local ok, icons = pcall(require, "mini.icons")
+local _state = {
+  buf = -1,
+  lsp = {
+    name = "",
+    nr = 0,
+  },
+}
 
 local function file_icon_and_name()
+  local ok, icons = pcall(require, "mini.icons")
   local fname = vim.fn.expand("%:t")
   if fname:len() > 0 then
     if ok then
@@ -13,53 +20,64 @@ local function file_icon_and_name()
   return "[No Name]"
 end
 
-local function language_info()
-  local ft = vim.bo.filetype
-  if ft:len() == 0 then
-    return ""
-  end
-
-  local clients = vim.lsp.get_clients({ bufnr = 0 })
-  local lsp_names = vim
-    .iter(clients)
+local function refresh_lsp(bufnr)
+  local lsp = vim.lsp.get_clients({ bufnr = bufnr })
+  local name = vim
+    .iter(lsp)
+    :rev()
     :map(function(c)
       return c.name
     end)
-    :totable()
+    :next()
 
-  local parts = { ft }
-  if #lsp_names > 0 then
-    table.insert(parts, string.format("%%#NonText#@%%#Special#%s%%*", lsp_names[1]))
-  end
-
-  return string.format("[%s]", table.concat(parts, ""))
+  return name, #lsp
 end
 
-local function diagnostic_count()
-  local status = {}
-  local diags = vim.diagnostic.count(0)
-  for id, count in pairs(diags) do
-    local severity = vim.diagnostic.severity[id]
-    local label = string.format("%%#Diagnostic%s#%s:%d%%*", severity, severity:sub(1, 1), count)
-    vim.list_extend(status, { label })
+local function lsp_info()
+  local bufnr = vim.fn.bufnr()
+
+  if _state.buf ~= bufnr then
+    _state.lsp.name, _state.lsp.nr = refresh_lsp(bufnr)
+    _state.buf = bufnr
   end
-  return table.concat(status, " ")
+
+  local clients = _state.lsp.nr
+  local main_lsp = _state.lsp.name
+  local parts = {}
+  table.insert(parts, string.format("%%#Special#%s%%*", main_lsp))
+
+  if clients > 1 then
+    table.insert(parts, "...")
+  end
+
+  local out = ""
+  if main_lsp and #main_lsp > 0 then
+    out = string.format("[%s]", table.concat(parts, ""))
+  end
+
+  return out
 end
 
 function StatusLine.tostring()
-  local file_info = file_icon_and_name()
-  local labels = diagnostic_count()
-  local padding = 2 + vim.api.nvim_eval_statusline(labels, { highlights = false }).width
-  local diagnostics = string.format("%%-%d.(%s%%)", padding, labels)
-  local language = language_info()
+  local file_info = file_icon_and_name() .. " %#WarningMsg#%m%*"
+  local language = lsp_info()
   return table.concat({
     "%<",
-    file_info .. " %#WarningMsg#%m%*",
+    file_info,
     "%=",
-    diagnostics,
+    vim.diagnostic.status() .. " ",
     language,
     "%12.(%l%#NonText#:%*%L%)",
   })
 end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ctx)
+    if ctx.buf == vim.fn.bufnr() then
+      _state.lsp.name, _state.lsp.nr = refresh_lsp(ctx.buf)
+    end
+  end,
+  once = true,
+})
 
 return StatusLine
